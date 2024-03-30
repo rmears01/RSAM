21ef888dff5f729a8961a2f2242858b677596d93
rmears01 <164239418+rmears01@users.noreply.github.com>
2024-03-30 18:33:07 +0000

    added rsa signature verification

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ app = Flask(__name__)
 
 def main():
     choice = input("generate new keyset? (y/n):")
+    
     if choice.lower() == "y":
+        #create new keyset
         keys = generate_keys()
         with open("public.pem", "wb") as file:
             file.write(keys[0].save_pkcs1("PEM"))
@@ -23,22 +25,46 @@ def main():
         with open("private.pem", "wb") as file:
             file.write(keys[1].save_pkcs1("PEM"))
             file.close()
-    else:
-        with open("public.pem", "rb") as file:
-            public_key = rsa.PublicKey.load_pkcs1(file.read())
-            file.close()
+
+    #read the keys from the files
+    with open("public.pem", "rb") as file:
+        public_key = rsa.PublicKey.load_pkcs1(file.read())
+        file.close()
+    with open("private.pem", "rb") as file:
+        private_key = rsa.PrivateKey.load_pkcs1(file.read())
+        file.close()
+
+    server = input("url or ip of the recieving client:")
+    while True:
+        #get your private key
         with open("private.pem", "rb") as file:
             private_key = rsa.PrivateKey.load_pkcs1(file.read())
             file.close()
 
-    server = input("url or ip of the recieving client:")
-    while True:
+        #get the message from the user
         message = input("message to send:")
         public_key = rsa.PublicKey.load_pkcs1(requests.get(server + "/getKey").text)
-        with open("message", "wb") as file:
-            file.write(encrypt(message, public_key))
+       
+        #create and save the signature
+        signature = rsa.sign(message.encode(), private_key, 'SHA-512')
+        with open("signature", "wb") as file:
+            file.write(signature)
             file.close()
-        files = {'file': open('message','rb')}
+
+        #save encrypted message
+        message = encrypt(message, public_key)
+        with open("message", "wb") as file:
+            file.write(message)
+            file.close()
+
+        
+        
+        
+        files = {
+                'message': open('message','rb'),
+                'signature': open('signature','rb'),
+                'public key': open('public.pem','rb')
+                }
         requests.post(server + "/recv", files=files)
 
         
@@ -57,19 +83,38 @@ def getKey():
 
 @app.route('/recv', methods=["POST"])
 def recv():
-    files = request.files["file"]
-    files.save("message")
+    #save the files
+    messageFile = request.files["message"]
+    signatureFile = request.files["signature"]
+    pubKeyFile = request.files["public key"]
+    messageFile.save("message")
+    signatureFile.save("signature")
+    pubKeyFile.save("recv-public.pem")
+
+    #decrypt the message
     with open("private.pem", "rb") as file:
         private_key = rsa.PrivateKey.load_pkcs1(file.read())
         file.close()
     with open("message", "rb") as file:
         message = decrypt(file.read(), private_key)
         file.close()
-    
-    with open(f"messages.log", "a") as file:
-        file.write(message + "\n")
+
+    #check the signature
+    with open("signature", "rb") as file:
+        signature = file.read()
         file.close()
-    return "success"
+    with open("recv-public.pem", "rb") as file:
+        recieved_public_key = rsa.PublicKey.load_pkcs1(file.read())
+        file.close()
+    if rsa.verify(message.encode(), signature, recieved_public_key) == "SHA-512":
+        #write the message to messages.log
+        with open("messages.log", "a") as file:
+            file.write(message + "\n")
+            file.close()
+        return "success"
+    else:
+        print("fail")
+        return "fail"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
